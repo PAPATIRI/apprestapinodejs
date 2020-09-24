bb9731358a01b4536d23e4584703f332d7574ab2
PAPATIRI <syariftaufik11@gmail.com>
2020-09-25 04:44:43 +0700

    menambahkan file koneksi ke database server.js koneksi.js

---
+++ koneksi.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+
+//membuat koneksi ke database
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'restapi'
+})
+
+conn.connect((err) => {
+    if (err) throw err;
+    console.log('mysql terkoneksi');
+});
+
+module.exports = conn;
